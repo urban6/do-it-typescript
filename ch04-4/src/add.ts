5059c611f0579cb10328cc52e2db0afabc3c9c4d
전승철 <32761829+urban6@users.noreply.github.com>
2022-05-26 14:43:18 +0000

    [22/05/26] 4장 커밋

---
+++ ch04-4/src/add.ts
@@ -0,0 +1,9 @@
+export type NumberToNumberFunc = (number) => number
+
+export const add =(a: number): NumberToNumberFunc => {
+  const _add: NumberToNumberFunc = (b: number): number => {
+    return a + b
+  }
+
+  return _add
+}
---
+++ ch04-5/src/return-object.ts
@@ -0,0 +1,11 @@
+export type Person = {name: string, age: number}
+
+export const makePerson = (name: string, age: number = 10): Person => {
+  const person = {name, age}
+  return person
+}
+
+ console.log(makePerson('Jack'));
+ console.log(makePerson('Jane', 33));
+ 
+ 
